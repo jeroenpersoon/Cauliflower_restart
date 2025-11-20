c8a205df2c19505c1ed99f894c81e502375704e0
jeroenpersoon <jeroen.persoon@wur.nl>
2025-11-20 16:55:01 +0100

    constructing variation table for cauliflower pangenome and add variants to pangenome

---
+++ cauliflower/T22cauliflower4/pangenome/mapping.sh
@@ -0,0 +1,4 @@
+pantools map pangenome_4_DB ../../../../reseq/caniego_F1_R1.fq ../../../../reseq/caniego_F1_R2.fq
+pantools map pangenome_4_DB ../../../../reseq/liria_F1_R1.fq ../../../../reseq/liria_F1_R2.fq
+pantools map pangenome_4_DB ../../../../reseq/TKI-0143_R1_001.fastq ../../../../reseq/TKI-0143_R2_001.fastq
+pantools map pangenome_4_DB ../../../../reseq/TKI-0155_R1_001.fastq ../../../../reseq/TKI-0155_R2_001.fastq
